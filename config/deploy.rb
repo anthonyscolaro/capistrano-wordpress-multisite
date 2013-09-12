1ba9d3b0c3a660c0d8e1be905fe12475d861c7e8
Anthony Scolaro <anthonys@projectassistant.org>
2013-09-12 05:44:24 -0700

    move some things around for better organization

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "projectassistant"
+set :application, ""
 default_run_options[:pty] = true
 
 set :stages, %w(staging production)
@@ -8,17 +8,16 @@ require 'capistrano/ext/multistage'
 set :scm, :subversion
 set :normalize_asset_timestamps, false
 
+# for permissions tasks
+set :user, ""
+set :group, ""
+
 namespace(:symlink) do
   task :wpconfig, :roles => :app do
     run <<-CMD
       ln -nfs #{release_path}/../../shared/system/wp-config.php #{release_path}/public/wp-config.php
     CMD
   end
-#  task :htaccess, :roles => :app do
-#    run <<-CMD
-#      ln -nfs #{release_path}/../../shared/.htaccess #{release_path}/public/.htaccess
-#    CMD
-#  end
   task :uploads, :roles => :app do
     run <<-CMD
       ln -nfs #{release_path}/../../shared/uploads #{release_path}/public/wp-content/uploads 
@@ -52,7 +51,6 @@ namespace(:symlink) do
 end
 
 after "deploy:update_code", "symlink:wpconfig"
-#after "symlink:wpconfig", "symlink:htaccess"
 after "symlink:wpconfig", "symlink:uploads"
 after "symlink:uploads", "symlink:blogsdir"
 after "symlink:blogsdir", "symlink:timcache"
@@ -60,36 +58,49 @@ after "symlink:timcache", "symlink:w3cache"
 after "symlink:w3cache", "symlink:w3config"
 after "symlink:w3config", "symlink:nginxconfig"
 
+namespace :permissions do
+  task :ownership, :roles => :app do
+    run <<-CMD
+      chown -R #{user}:#{group} #{release_path}
+    CMD
+  end
+  task :file, :roles => :app do
+    run <<-CMD
+      find #{release_path} -type f -exec chmod 664 {} ';'
+    CMD
+  end
+    task :directory, :roles => :app do
+    run <<-CMD
+      find #{release_path} -type d -exec chmod 775 {} ';'
+    CMD
+  end
+end
+
+after "deploy:restart", "permissions:ownership"
+after "permissions:ownership", "permissions:file"
+after "permissions:file", "permissions:directory"
+
+# if you want to restart/reload NGINX on each deploy
+
 namespace :nginx do
   [:stop, :start, :restart, :reload].each do |action|
-    desc "#{action.to_s.capitalize} Apache"
+    desc "#{action.to_s.capitalize} nginx"
     task action, :roles => :web do
       invoke_command "/etc/init.d/nginx #{action.to_s}", :via => run_method
     end
   end
 end
 
-namespace :permissions do
-  task :ownership, :roles => :app do
-    run <<-CMD
-      chown -R 04639-ssh:www-data /var/www/createmyid
-    CMD
-  end
-  task :file, :roles => :app do
-    run <<-CMD
-      find /var/www/createmyid/web/ -type f -exec chmod 664 {} ';'
-    CMD
-  end
-    task :directory, :roles => :app do
-    run <<-CMD
-      find /var/www/createmyid/web/ -type d -exec chmod 775 {} ';'
-    CMD
-  end
-end
-
-after "deploy:w3cache", "nginx:permissions"
+after "deploy:restart", "nginx:reload"
 
 # if you want to clean up old releases on each deploy uncomment this:
-
 #after "deploy:restart", "deploy:cleanup"
-#after "deploy:cleanup", "nginx:reload"
+
+
+# this was commented out after switching to nginx
+#  task :htaccess, :roles => :app do
+#    run <<-CMD
+#      ln -nfs #{release_path}/../../shared/.htaccess #{release_path}/public/.htaccess
+#    CMD
+#  end
+#after "symlink:wpconfig", "symlink:htaccess"
